771ca1bb0c4e0a73a7ba1f2f7f4463e51672c344
ErrXa <ErrXa@DESKTOP-5KE91AE>
2025-03-29 19:27:15 +0100

    Tercera versión.

--- ProXabiMari/src/paq1/Main.java
+++ ProXabiMari/src/paq1/Main.java
@@ -3,6 +3,8 @@ package paq1;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		System.out.println("Buenas tardes");
 		System.out.println("Adiós Mundo");
+		
 	}
 }
