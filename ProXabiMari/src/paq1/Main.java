cbb80af460c4489dc0ddc26fbdd64d3f0a27dffb
ErrXa <ErrXa@DESKTOP-5KE91AE>
2025-03-29 19:01:35 +0100

    Segunda versión.

--- ProXabiMari/src/paq1/Main.java
+++ ProXabiMari/src/paq1/Main.java
@@ -2,6 +2,7 @@ package paq1;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo"):
+		System.out.println("Hola Mundo");
+		System.out.println("Adiós Mundo");
 	}
 }
