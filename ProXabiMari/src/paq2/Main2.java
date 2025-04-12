3ba33bbe3129281754f3fe41d248e4393c8c94f6
ErrXa <ErrXa@DESKTOP-5KE91AE>
2025-04-12 18:38:51 +0200

    Cambio nueva rama

---
+++ ProXabiMari/src/paq2/Main2.java
@@ -0,0 +1,11 @@
+package paq2;
+
+public class Main2 {
+
+	    public static void main(String[] args) {
+	        System.out.println("Esta es la rama del paso 18");
+	        
+	        System.out.print("Cambio en el paso 21");
+	    }
+	
+}
